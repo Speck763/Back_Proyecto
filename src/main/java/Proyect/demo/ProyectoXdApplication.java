88c54e39c06cd242fa265a394c2f5bc3a9a08a2e
Fernandez  Berna <141880472+Speck763@users.noreply.github.com>
2023-11-27 13:14:41 -0300

    Add files via upload

---
+++ src/main/java/Proyect/demo/ProyectoXdApplication.java
@@ -0,0 +1,13 @@
+package Proyect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoXdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoXdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyect/demo/ProyectoXdApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyect.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoXdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
