42dace4ca805526ca4233d0a4486a5af59a75066
ivannmolina <ivan14molina@gmail.com>
2023-11-30 17:26:38 -0300

    /commit/ pago pendiente (taza de interes 70% cada dia) : alfajor

--- src/main/java/Proyect/demo/ProyectoXdApplication.java
+++ src/main/java/Proyect/demo/ProyectoXdApplication.java
@@ -1,13 +1,21 @@
 package Proyect.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProyectoXdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoXdApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init() {
+		return args -> {
+			System.out.println("/-/-/-/--- * ESTOY FUNCIONANDO * ---/-/-/-/");
+		};
 	}
 
 }
