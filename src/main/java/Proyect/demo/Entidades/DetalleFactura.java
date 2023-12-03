5016e4bbfe1e4e939167d56483d437382e0e228b
Speck763 <bersilmanlik@gmail.com>
2023-12-02 23:09:28 -0300

    por  eso esperaba con la carita empapada

--- src/main/java/Proyect/demo/Entidades/DetalleFactura.java
+++ src/main/java/Proyect/demo/Entidades/DetalleFactura.java
@@ -23,6 +23,6 @@ public class DetalleFactura extends Base{
     private double subTotalLinea;
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "fk_detale_factura")
+    @JoinColumn(name = "fk_producto")
     private Producto producto;
 }
