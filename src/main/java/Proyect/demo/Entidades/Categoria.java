0a7a87f71d883190aa0cb427fb76148ccd953609
Speck763 <bersilmanlik@gmail.com>
2023-12-19 16:32:49 -0300

    Estoy cansado jefe 2

--- src/main/java/Proyect/demo/Entidades/Categoria.java
+++ src/main/java/Proyect/demo/Entidades/Categoria.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -16,11 +18,25 @@ import java.util.List;
 
 @Table(name = "Categoria")
 public class Categoria extends Base{
-
+    @NotNull
     @Column(name = "nombreCategoria")
-    private String nombreCaegoria;
+    private String nombreCategoria;
 
-    private String imagen;
+    @NotNull
+    @Column(name = "url_imagen")
+    private String urlImagen;
+
+    //comentario anti cbum
+    @NotNull
+    @Column(name = "fecha_alta")
+    @Temporal(TemporalType.DATE)
+    private Date fechaAlta;
+
+    //private Date fechaModificacion;
+
+    @Column(name = "fecha_baja")
+    @Temporal(TemporalType.DATE)
+    private Date fechaBaja;
 
     @OneToMany(mappedBy = "fk_producto")
     private List<Producto> productos;
