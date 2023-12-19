efa1c176538534e2badbb694b147e51ae719c025
ivannmolina <ivan14molina@gmail.com>
2023-12-18 21:57:30 -0300

    /commit/ cambios en categoria

--- src/main/java/Proyect/demo/Repository/CategoriaRepository.java
+++ src/main/java/Proyect/demo/Repository/CategoriaRepository.java
@@ -2,6 +2,7 @@ package Proyect.demo.Repository;
 
 import Proyect.demo.Entidades.Categoria;
 import Proyect.demo.Entidades.Domicilio;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -11,9 +12,10 @@ import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends BaseRepository<Categoria, Long> {
-
-    @Query(value = "SELECT ca FROM Categoria ca WHERE ca.nombreCategoria like '%1%'")
+    @Transactional
+    @Query(value = "SELECT ca FROM Categoria ca WHERE ca.nombreCategoria like %?1%")
     List<Categoria> search(String filtro);
-    @Query(value= "SELECT ca FROM Categoria ca WHERE ca.nombreCategoria like '%1%'")
+    @Transactional
+    @Query(value= "SELECT ca FROM Categoria ca WHERE ca.nombreCategoria like %?1%")
     Page<Categoria> search(String filtro, Pageable pageable);
 }
